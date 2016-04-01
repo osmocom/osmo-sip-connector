c313ab0f6b50fc7d2bd066099624fc0a6a4454ac
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2016-04-01 15:42:41 +0200

    mncc: Only copy 15 chars to leave the last one as NULL
    Right now we are lucky the next byte after imsi is already set
    to NUL here but we should not rely on that.

--- src/mncc.c
+++ src/mncc.c
@@ -656,7 +656,7 @@ int mncc_create_remote_leg(struct mncc_connection *conn, struct call *call,
 	memcpy(&mncc.calling.number, calling, sizeof(mncc.calling.number));
 
 	if (conn->app->use_imsi_as_id) {
-		snprintf(mncc.imsi, 16, called);
+		snprintf(mncc.imsi, 15, called);
 	} else {
 		mncc.fields |= MNCC_F_CALLED;
 		mncc.called.plan = 1;
