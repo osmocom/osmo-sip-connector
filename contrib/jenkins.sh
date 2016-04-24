5f4c6f3a0420029c66776ab703315628d547f91b
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2016-04-24 22:32:49 +0200

    jenkins: Try to enable the vty tests as well

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -14,7 +14,7 @@ $MAKE $PARALLEL_MAKE install
 
 cd ../../
 autoreconf --install --force
-PKG_CONFIG_PATH=$PWD/deps/install/lib/pkgconfig ./configure
+PKG_CONFIG_PATH=$PWD/deps/install/lib/pkgconfig ./configure --enable-vty-tests --enable-external-tests
 PKG_CONFIG_PATH=$PWD/deps/install/lib/pkgconfig $MAKE $PARALLEL_MAKE
 PKG_CONFIG_PATH=$PWD/deps/install/lib/pkgconfig LD_LIBRARY_PATH=$PWD/deps/install/lib $MAKE distcheck
 
