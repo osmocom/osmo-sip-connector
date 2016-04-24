f896df6b8ed6b3751086d36e9738a143365f968c
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2016-04-24 22:24:44 +0200

    distcheck/tests: Add the referenced osmoappdesc.py for testing
    distcheck fails because the file is not present. Write it out to
    get our standard vty checking to work. Add simple Makefile and
    call it.

---
+++ osmoappdesc.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+# (C) 2016 by Holger Hans Peter Freyther
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+
+app_configs = {
+    "osmo-sip-connector": ["doc/examples/osmo-sip-connector.cfg"]
+}
+
+apps = [
+    (4256, "src/osmo-sip-connector", "OsmoMNCC", "osmo-sip-connector")
+]
+
+vty_command = ["./src/osmo-sip-connector", "-c",
+               "doc/examples/osmo-sip-connector.cfg"]
+vty_app = apps[0]
