e4bd8d42e98cadab453f6a20c4b395c56e713ed8
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2016-04-25 08:41:19 +0200

    jenkins: Make it find the libosmocore library as well

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -16,6 +16,6 @@ cd ../../
 autoreconf --install --force
 PKG_CONFIG_PATH=$PWD/deps/install/lib/pkgconfig ./configure --enable-vty-tests --enable-external-tests
 PKG_CONFIG_PATH=$PWD/deps/install/lib/pkgconfig $MAKE $PARALLEL_MAKE
-PKG_CONFIG_PATH=$PWD/deps/install/lib/pkgconfig $MAKE check
+PKG_CONFIG_PATH=$PWD/deps/install/lib/pkgconfig LD_LIBRARY_PATH=$PWD/deps/install/lib $MAKE check
 PKG_CONFIG_PATH=$PWD/deps/install/lib/pkgconfig LD_LIBRARY_PATH=$PWD/deps/install/lib $MAKE distcheck
 
