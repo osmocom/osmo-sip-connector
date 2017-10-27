4abdb313e3989557cd7dc546da59db8ac2180422
Neels Hofmeyr <neels@hofmeyr.de>
2017-10-27 22:54:50 +0200

    jenkins: use osmo-clean-workspace.sh before and after build
    See osmo-ci change I2409b2928b4d7ebbd6c005097d4ad7337307dd93 for rationale.
    
    Depends: I2409b2928b4d7ebbd6c005097d4ad7337307dd93
    Change-Id: I7e9f19755f55b274def9d757f72f2a08a01a4cce

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -7,8 +7,9 @@ deps="$base/deps"
 inst="$deps/install"
 export deps inst
 
+osmo-clean-workspace.sh
+
 mkdir "$deps" || true
-rm -rf "$inst"
 
 osmo-build-dep.sh libosmocore
 
@@ -32,3 +33,5 @@ $MAKE check \
   || cat-testlogs.sh
 $MAKE distcheck \
   || cat-testlogs.sh
+
+osmo-clean-workspace.sh
