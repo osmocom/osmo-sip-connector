0241f7be954fdeac38d937c8fbe626a53fbcb61b
Oliver Smith <osmith@sysmocom.de>
2018-11-30 16:29:45 +0100

    contrib/jenkins.sh: build and publish manuals
    Add new environment variables WITH_MANUALS and PUBLISH to control if
    the manuals should be built and uploaded. Describe all environment vars
    on top of the file.
    
    When WITH_MANUALS is set, install osmo-gsm-manuals like any other
    dependency and add --enable-manuals to the configure flags (for "make"
    and "make distcheck"). Add the bin subdir of the installed files to
    PATH, so osmo-gsm-manuals-check-depends can be used by ./configure.
    
    Related: OS#3385
    Change-Id: I4cf9d3c21f3912eac3c51bae1ac7b2ad0845c947

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+# jenkins build helper script for osmo-sip-connector.  This is how we build on jenkins.osmocom.org
+#
+# environment variables:
+# * WITH_MANUALS: build manual PDFs if set to "1"
+# * PUBLISH: upload manuals after building if set to "1" (ignored without WITH_MANUALS = "1")
+#
 
 set -ex
 
@@ -17,6 +23,14 @@ verify_value_string_arrays_are_terminated.py $(find . -name "*.[hc]")
 
 export PKG_CONFIG_PATH="$inst/lib/pkgconfig:$PKG_CONFIG_PATH"
 export LD_LIBRARY_PATH="$inst/lib"
+export PATH="$inst/bin:$PATH"
+
+# Additional configure options and depends
+CONFIG=""
+if [ "$WITH_MANUALS" = "1" ]; then
+	osmo-build-dep.sh osmo-gsm-manuals
+	CONFIG="--enable-manuals"
+fi
 
 set +x
 echo
@@ -27,11 +41,15 @@ echo
 set -x
 
 autoreconf --install --force
-./configure --enable-werror --enable-vty-tests --enable-external-tests
+./configure --enable-werror --enable-vty-tests --enable-external-tests $CONFIG
 $MAKE $PARALLEL_MAKE
 $MAKE check \
   || cat-testlogs.sh
-$MAKE distcheck \
+DISTCHECK_CONFIGURE_FLAGS="$CONFIG" $MAKE distcheck \
   || cat-testlogs.sh
+
+if [ "$WITH_MANUALS" = "1" ] && [ "$PUBLISH" = "1" ]; then
+	make -C "$base/doc/manuals" publish
+fi
 
 osmo-clean-workspace.sh
